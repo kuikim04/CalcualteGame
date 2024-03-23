8f1deb3ef91400c197134d63219ab4a9c069094d
Kuikim04 <133838533+Kuikim044@users.noreply.github.com>
2024-03-24 00:17:27 +0700

    Update

--- Calculate Game/Assets/Scripts/PlayerData.cs
+++ Calculate Game/Assets/Scripts/PlayerData.cs
@@ -6,8 +6,8 @@ public class PlayerData : ScriptableObject
     public int Coin;
     public int BestScore;
 
-    public int Item1;
-    public int Item2;
-    public int Item3;
-    public int Item4;
+    public int Item1 = 5;
+    public int Item2 = 5;
+    public int Item3 = 5;
+    public int Item4 = 5;
 }
